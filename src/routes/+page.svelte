120229ce7a9f53b404e6c81cf3a3d3cae3a089a1
Xiao Fan <2258776+NAR8789@users.noreply.github.com>
2023-08-16 11:55:25 -0700

    implement the basic split timer functionality

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,42 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import {onDestroy} from 'svelte'
+
+  let intervals:number[] = []
+  let lastSplit = Date.now()
+  let now = Date.now()
+
+  $:interval = now - lastSplit
+  $:working = intervals.length % 2 === 1
+
+  if (typeof requestAnimationFrame !== 'undefined') {
+    let frame:number;
+    (function tick() {
+      now = Date.now()
+      frame = requestAnimationFrame(tick)
+    }())
+
+    onDestroy(() => {
+      cancelAnimationFrame(frame)
+    })
+  }
+
+  function split() {
+    now = Date.now()
+    intervals = [now - lastSplit, ...intervals]
+    lastSplit = now
+  }
+</script>
+
+<h1>Pomodoro Inversion</h1>
+
+<div>{interval}</div>
+
+<button on:click={split}>{ working ? "rest" : "work" }</button>
+
+<h2>cycle history</h2>
+{ #each intervals as interval, i }
+  <div>
+    <span>{(intervals.length - i) % 2 === 0 ? "work" : "rest" }</span>
+    <span>{interval}</span>
+  </div>
+{ /each }
